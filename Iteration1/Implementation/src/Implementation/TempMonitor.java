8edc2396c001f7f613e600da768138ab00c94159
ckdu <51294469+ckdu@users.noreply.github.com>
2023-10-29 15:59:31 -0400

    Constructor

--- Iteration1/Implementation/src/Implementation/TempMonitor.java
+++ Iteration1/Implementation/src/Implementation/TempMonitor.java
@@ -1,5 +1,11 @@
 package Implementation;
 
 public class TempMonitor {
-
+	private SensorRegistry sensorRegistry;
+	private LocationRegistry locationRegistry;
+	
+	public TempMonitor(SensorRegistry sensorRegistry, LocationRegistry locationRegistry) {
+		this.sensorRegistry = sensorRegistry;
+		this.locationRegistry = locationRegistry;
+	}
 }
