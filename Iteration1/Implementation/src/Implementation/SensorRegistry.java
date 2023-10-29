eb26bb630883db379040b7c9efe8e23e9dfc16f1
hisham-kitaz <hishamo-hhk@hotmail.com>
2023-10-29 15:41:21 -0400

    Adding Sensor Registry Class Hashmaps. added getTemperatureFromSensor added create

---
+++ Iteration1/Implementation/src/Implementation/SensorRegistry.java
@@ -0,0 +1,48 @@
+package Implementation;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class SensorRegistry {
+	
+	private List<Sensor> sensors;
+	
+	
+	//	ID ---> Sensor HashMap
+	HashMap<Integer, Sensor> sensorDict = new HashMap<Integer, Sensor>();
+	
+	
+	//	Location ---> Sensor HashMaps
+	HashMap<Sensor, Location> sensorLocationPairs = new HashMap<Sensor, Location>();
+	
+	HashMap<Sensor, Temperature> sensorTemperaturePairs = new HashMap<Sensor, Temperature>();
+
+	
+	public void addSensor(Sensor sensor) {
+	}
+	
+	public Sensor getSensor(int sensorId){
+		if (!sensorDict.containsKey(sensorId)) {
+			System.out.println("Invalid sensor ID");
+			return null;
+		}
+		
+		Sensor sensor = sensorDict.get(sensorId);
+		if (sensor.getIsDeployed()) {
+			System.out.println("Sensor is already deployed.");
+			return null;
+		}
+		return sensor;
+	}
+	
+	public Temperature getTemperatureFromSensor(Sensor sensor) {
+		return sensorTemperaturePairs.get(sensor);
+	}
+		
+	public void create(Sensor sensor, Location location) {
+		sensorLocationPairs.put(sensor, location);
+	}
+
+
+
+}
