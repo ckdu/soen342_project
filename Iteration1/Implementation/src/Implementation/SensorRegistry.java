024d0ce621f539ec380bd407d63f1dcd98b99e79
hisham-kitaz <hishamo-hhk@hotmail.com>
2023-10-29 15:49:42 -0400

    Adding SensorRegistry class, hashmpas and addsensor method

--- Iteration1/Implementation/src/Implementation/SensorRegistry.java
+++ Iteration1/Implementation/src/Implementation/SensorRegistry.java
@@ -18,7 +18,22 @@ public class SensorRegistry {
 	HashMap<Sensor, Temperature> sensorTemperaturePairs = new HashMap<Sensor, Temperature>();
 
 	
+	public void addSensor(Sensor sensor) {
+	}
 	
+	public Sensor getSensor(int sensorId){
+		if (!sensorDict.containsKey(sensorId)) {
+			System.out.println("Invalid sensor ID");
+			return null;
+		}
+		
+		Sensor sensor = sensorDict.get(sensorId);
+		if (sensor.getIsDeployed()) {
+			System.out.println("Sensor is already deployed.");
+			return null;
+		}
+		return sensor;
+	}
 	
 	public Temperature getTemperatureFromSensor(Sensor sensor) {
 		return sensorTemperaturePairs.get(sensor);
