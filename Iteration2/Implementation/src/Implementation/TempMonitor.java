0ffc2959518cc1551c8183f523044906f7c7de75
Etienne Paquet <paquetetienne@gmail.com>
2023-11-19 18:19:36 -0500

    added GetAllLocationsAndTemperatures() method in TempMonitor

--- Iteration2/Implementation/src/Implementation/TempMonitor.java
+++ Iteration2/Implementation/src/Implementation/TempMonitor.java
@@ -1,5 +1,9 @@
 package Implementation;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+
 public class TempMonitor {
 	private SensorRegistry sensorRegistry;
 	private LocationRegistry locationRegistry;
@@ -33,11 +37,80 @@ public class TempMonitor {
 		} 
 		else {
             // Add the sensor and the location to their respective hashMaps
-			sensorRegistry.create(sensor, location);
+			sensorRegistry.createSensorLocationPair(sensor, location);
 			locationRegistry.create(sensor, location);
 			
 			sensor.setIsDeployed(true);
 			System.out.println("Sensor has been deployed.");
 		}
 	}
+    
+    public void replaceSensor(int sensorId, int locationId) {
+    	
+        // Get the sensor from the registry
+		Sensor newSensor = sensorRegistry.getSensor(sensorId);
+		
+        // Get the location from the registry
+		Location location = locationRegistry.getLocationForReadTemperature(locationId);
+
+        //	check that the new sensor and location are valid
+		if (newSensor == null)  {
+			System.out.println("Error: New sensor doesn't exist");
+			return;
+		} 
+		
+		if (location == null)  {
+			System.out.println("Error: Location doesn't exist");
+			return;
+		} 
+
+		// Check that the sensor is not already deployed
+		if (newSensor.getIsDeployed()) {
+			System.out.println("Error: Sensor to deploy is already delpoyed.");
+			return;
+		}
+		
+		// Grab old sensor from location
+		Sensor oldSensor = locationRegistry.getSensorFromLocation(location);
+		// Save the old sensor's temperature
+		Temperature temp = sensorRegistry.getTemperatureFromSensor(oldSensor);
+	
+		// Replace the old sensor by the new sensor in locationRegistry
+		if (locationRegistry.ReplaceSensorInLocation(location, newSensor) == null) {
+			System.out.println("Error: No sensor in location");
+			return;
+		}
+		
+		// Remove old sensor from HashMaps
+		sensorRegistry.remove(oldSensor);
+				
+		// Add new sensor to SensorDict
+		sensorRegistry.sensorDict.put(Collections.max(sensorRegistry.sensorDict.keySet()) + 1,  newSensor);
+		
+		// Add new sensor to SensorRegistry
+		sensorRegistry.createSensorLocationPair(newSensor, location);
+		sensorRegistry.createSensorTemperaturePair(newSensor, temp);
+		
+		newSensor.setIsDeployed(true);
+		System.out.println("Sensor " + newSensor.toString() + " has been deployed to location " + location.toString() + ".");
+	}
+
+    public void GetAllLocationsAndTemperatures() {
+    	Collection<Sensor> sensors = sensorRegistry.sensorDict.values();
+    	for (Sensor s: sensors) {
+    		if (!s.getIsDeployed()) {continue;}
+    		
+    		Location l = sensorRegistry.getLocationFromSensor(s);
+    		Temperature t = sensorRegistry.getTemperatureFromSensor(s);
+    		System.out.println("Location " + l.locationId + " has temperature: " + t.toString());
+    	}
+    	
+    }
+    
+    
+    
+    
+
+    
+    
 }
